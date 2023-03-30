e28661c469964f66a0f54dce2e87e28df898d931
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2023-03-30 16:12:24 +0200

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import platform
+import subprocess
+
+
+PYTHON_DEPENDENCIES = "tk "
+
+OS_TYPE = platform.system()
+PROGNAME = "Chess Royal"
+RELEASE = "Revision 1.0"
+AUTHOR= "(c) On mettra nos nom plus tard"
+
+def pip_dep():
+    pip_install = "pip3 install " + PYTHON_DEPENDENCIES
+    process = subprocess.Popen(pip_install.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    if(error):
+        print("PIP INSTALL FAILED. You may install depedencies yourself. See error :")
+        print(error)
+
+def install_mac():
+     print("No extra dep for the moment")
+
+def install_win():
+     print("No extra dep for the moment")
+
+print(RELEASE + " | " + AUTHOR)
+pip_dep() # pip3 install of all python lib
+if(OS_TYPE == 'Darwin'):
+    print("Mac OS detected")
+    install_mac()
+elif(OS_TYPE == 'Windows'):
+    print("Windows detected")
+    install_win()
+
+
