2bb57ee7fb1b1d2683ceb733a765932c48cc242b
Enzo GARNIER-GULINO <enzogarniergulino@gmail.com>
2023-03-31 06:17:28 +0200

    Added a sound

--- engine/board.py
+++ engine/board.py
@@ -20,7 +20,7 @@ class Board:
             self.board[1][i] = Pawn(1, [1, i])
             self.board[6][i] = Pawn(0, [6, i])
 
-        self.board[4][4] = Rook(1, [4, 4]) # 0, 0
+        self.board[4][4] = Rook(1, [4, 4]) # 0 0
         self.board[7][0] = Rook(0, [7, 0])
 
         self.board[0][1] = Knight(1, [0, 1]) 
@@ -32,7 +32,7 @@ class Board:
         self.board[0][3] = Queen(1, [0, 3])
         self.board[7][3] = Queen(0, [7, 3])
 
-        self.board[0][4] = King(1, [0, 4])
+        self.board[0][4] = King(1, [0, 4]) 
         self.board[7][4] = King(0, [7, 4])
 
         self.board[0][5] = Bishop(1, [0, 5])
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from tkinter import *
 from engine.board import Board
+import playsound # pip install playsound==1.2.2
+
+def sound():
+    playsound.playsound('sounds\ChessMove1.mp3')
 
 print("Call constructor")
 board = Board()
 
 board.print_board()
+
+
 print("#####################################################")
 print(board.play(0, [0, 6], [0, 5]))
 print("\n\nMOVE\n\n")
 board.print_board()
+sound()
 
 print("#####################################################")
-print(board.play(1, [4, 4], [3, 4]))
+print(board.play(1, [4, 4], [0, 4]))
 print("MOVE\n\n")
 board.print_board()
+sound()
