074bb5f0db94f34653e7a17e5a90b6bb3f719a9a
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2023-03-31 06:11:20 +0200

    Fix bishop move

--- engine/piece/bishop.py
+++ engine/piece/bishop.py
@@ -1,9 +1,30 @@
 from engine.piece.piece import Piece as p
 
-class Bishop:
+class Bishop(p):
     def __init__(self, team, pos):  # pos = 2, 5
         self.team = team
         self.pos = pos 
+
+    def is_legal(self, board, pos):
+        board_list = board.get_board()
+        print("Is bishop lega")
+        print("pos = [" + str(pos[0]) + ", " + str(pos[1]) + "]")
+        if(not super().is_legal(pos)):
+            print("Out of the broad")
+            return False
+        pos_1 = self.pos[1] 
+        for i in range(self.pos[0]+1, pos[0]):
+            
+            if(self.pos[1] > pos[1]):
+                print("Minus")
+                pos_1 -= 1
+            else:
+                print("Max")
+                pos_1 += 1
+            print("Checking pos ==> " + str(i) + " " + str(pos_1))
+            if(board_list[i][pos_1]):
+                return False
+        return True
 
     def get_playable_pos(self, board):
         pos_list = []
@@ -52,7 +73,8 @@ class Bishop:
         return pos_list
     
     def play(self, board, pos):
-        if(self.is_legal(board, pos)):
+        print("Bishop play")
+        if(not self.is_legal(board, pos)):
             return False
         self.pos = pos
         return True
--- main.py
+++ main.py
@@ -6,9 +6,18 @@ board = Board()
 
 board.print_board()
 print("#####################################################")
-print(board.play(1, [1, 0], [2, 0]))
+print(board.play(1, [1, 1], [2, 1]))
 print("\n\nMOVE\n\n")
 board.print_board()
 
 
+print("#####################################################")
+print(board.play(1, [0, 2], [2, 0]))
+print("\n\nMOVE\n\n")
+board.print_board()
 
+
+print("#####################################################")
+print(board.play(1, [2, 0], [7, 4]))
+print("\n\nMOVE\n\n")
+board.print_board()
